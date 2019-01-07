e7d813aa201bdad89319490304a91d04c24de414
Kinga Sieminiak <kinga.sieminiak@elpassion.pl>
2019-01-07 11:45:10 +0700

    Add webpack config and remove gulp

---
+++ buildScripts/build.js
@@ -0,0 +1,29 @@
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.babel';
+import chalk from 'chalk';
+
+process.env.NODE_ENV = 'production';
+
+console.log(chalk.blue('Generating minified bundle for production...'));
+
+webpack(webpackConfig).run((err, stats) => {
+  if(err) {
+    console.log(chalk.red(err));
+
+    return 1;
+  } else {
+    const jsonStats = stats.toJson();
+
+    if(jsonStats.hasErrors) {
+      return jsonStats.errors.map(error => console.log(chalk.red(error)));
+    }
+    
+    if(jsonStats.hasWarnings) {
+      jsonStats.warnings.map(warning => console.log(chalk.yellow(warning)));
+    }
+
+    console.log(`Webpack status: ${stats}`);
+    console.log(chalk.green('Your app has been built for production andwritten to /dist!'));
+    return 0;
+  }
+});
