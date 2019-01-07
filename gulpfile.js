ffd9fa3a421072c3294ebff7ab9e777da9ff62ae
Kinga Sieminiak <kinga.sieminiak@elpassion.pl>
2019-01-07 10:03:44 +0700

    Update packages

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function() {
-    return gulp.src('scss/style.scss')
+    return gulp.src('styles/style.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({
             errLogToConsole: true,
@@ -13,6 +13,13 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('css'))
 });
 
-gulp.task('watch', function(){
-    gulp.watch('scss/*.scss', ['sass']);
+gulp.task('watch', function() {
+  gulp.watch('scss/*.scss', ['sass']);
+});
+
+gulp.task('watch', function() {
+    gulp.watch('scss/*.scss')
+    .on('change', function(path, stats) {
+        console.log(path);
+    })
 });
